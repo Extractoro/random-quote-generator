6e33099c7be4aca9490ca6625fc512102be4b92c
Vadym <67538058+Extractoro@users.noreply.github.com>
2024-01-23 13:59:04 +0200

    add: init files, form and button

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SearchCategory from "./components/SearchCategory";
+
+function App() {
+  return (
+    <>
+      <SearchCategory />
+    </>
+  );
+}
+
+export default App;
